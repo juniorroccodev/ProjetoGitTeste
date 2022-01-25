80e4d6c6e76dc49b9d492659d10eb46ef00744db
junior rocco <norbertorocco@hotmail.com>
2022-01-25 08:27:24 -0300

    primeiro comite

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_git_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Hello");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("A");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("B");
+            }
+            finally
+            {
+                Console.WriteLine("C");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
