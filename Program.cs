7424d502c759e7a7e11fc4daf02e25797f7fb822
junior rocco <norbertorocco@hotmail.com>
2022-01-28 10:47:50 -0300

    ajuste-no-codigo

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ namespace projeto_git_teste
 
            int n1, n2, resultado;
 
-            Console.WriteLine("Digite qualquer número: ");
-            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out n1);
 
             Console.WriteLine("Digite o segundo número: ");
-            n2 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out n2);
 
             resultado = n1 + n2;
-            Console.WriteLine("O Resultado é: " + resultado);
+            Console.WriteLine($"O Resultado é: {resultado}");
 
             Console.ReadKey();
         }
